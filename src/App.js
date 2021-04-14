d7ffd62718266ce4180f1030a6badedeb0fc09b1
faraz3 <ch.faraz324@gmail.com>
2021-04-14 22:45:01 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ function App() {
             <div className="App container mx-auto relative">
                 <div className="flex text-4xl my-20 font-bold justify-center px-44">
                     <h1 className="pr-44">
-                        <Link to="/test2/">First Method</Link>
+                        <Link to="/">First Method</Link>
                     </h1>
                     <h1>
-                        <Link to="/test2/second">Second Method</Link>
+                        <Link to="/second">Second Method</Link>
                     </h1>
                 </div>
 
                 <Switch>
-                    <Route exact path="/test2/">
+                    <Route exact path="/">
                         <FirstMethod />
                     </Route>
-                    <Route exact path="/test2/second">
+                    <Route exact path="/second">
                         <SecondMethod />
                     </Route>
                 </Switch>
