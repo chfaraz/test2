54abd33e6dc831916795ac9d2a1294f4c81c5624
faraz3 <ch.faraz324@gmail.com>
2021-04-15 14:30:48 +0500

    fixed the routes issue

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ function App() {
             <div className="App container mx-auto relative">
                 <div className="flex text-4xl my-20 font-bold justify-center px-44">
                     <h1 className="pr-44">
-                        <Link to="/first">First Method</Link>
+                        <Link to="/faraz3/first">First Method</Link>
                     </h1>
                     <h1>
-                        <Link to="/second">Second Method</Link>
+                        <Link to="/faraz3/second">Second Method</Link>
                     </h1>
                 </div>
 
                 <Switch>
-                    <Route exact path="/first">
+                    <Route exact path="/faraz3/first">
                         <FirstMethod />
                     </Route>
-                    <Route exact path="/second">
+                    <Route exact path="/faraz3/second">
                         <SecondMethod />
                     </Route>
                 </Switch>
