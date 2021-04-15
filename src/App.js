d191f3e0048396c704e7bd62dfd7dbcf1e864d06
faraz3 <ch.faraz324@gmail.com>
2021-04-15 17:28:32 +0500

    working with object 2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,111 @@
 import React, { useState } from 'react';
-
+import Button from '@material-ui/core/Button';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
 import FirstMethod from './firstMethod';
 import SecondMethod from './secondMethod';
 
 function App() {
-    return (
-        <Router>
-            <div className="App container mx-auto relative">
-                <div className="flex text-4xl my-20 font-bold justify-center px-44">
-                    <h1 className="pr-44">
-                        <Link to="/test2/first">First Method</Link>
-                    </h1>
-                    <h1>
-                        <Link to="/test2/second">Second Method</Link>
-                    </h1>
-                </div>
+    const [popup, setPopup] = useState(false);
+    const [button, setButton] = useState(false);
+    const [state, setState] = useState([]);
+    const [text, setText] = useState('');
+    const [key, setKey] = useState('');
+    const [selection, setSelection] = useState('');
+    console.log(state);
 
-                <Switch>
-                    <Route exact path="/faraz3/first">
-                        <FirstMethod />
-                    </Route>
-                    <Route exact path="/faraz3/second">
-                        <SecondMethod />
-                    </Route>
-                </Switch>
+    // push to github and eploy to github pages
+    const mouseUp = function (e) {
+        var aaa = document.getElementById('aa');
+        var x = e.clientX;
+        var y = e.clientY;
+        var selection;
+
+        selection = window.getSelection();
+        var p = selection.toString();
+
+        setSelection(p);
+        // selection.toString() !== '' && alert(selection.toString() + e.pageX + '/' + e.pageY);
+        if (p.length > 0) {
+            aaa.style.display = 'block';
+            aaa.style.top = y - 100 + 'px';
+            aaa.style.left = x - 90 + 'px';
+        }
+        setKey(p);
+    };
+    const clicked = (e) => {
+        setPopup(true);
+        var ele = document.getElementById('aa');
+        ele.style.display = 'none';
+        // e.stopPropagation();
+    };
+    const setData = (e) => {
+        console.log(text);
+        console.log(key);
+        var d = new Date();
+        if (text !== '') {
+            setState(
+                state.concat({
+                    uuid: d.getTime() + Math.floor(Math.random() * 100) + Math.floor(Math.random() * 100),
+                    writtenText: text,
+                    selectedText: selection,
+                })
+            );
+
+            setPopup(false);
+        } else {
+            console.log('please enter some text');
+        }
+        var ele = document.getElementById('aa');
+        ele.style.display = 'none';
+        setText('');
+    };
+    // const hideButton = (e) => {
+    //     var ele = document.getElementById('aa');
+    //     ele.style.display = 'none';
+    //     e.stopPropagation();
+    // };
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const popupDiv = (
+        <div id="popup">
+            <h1>enter some text</h1>
+            <textarea value={text} onChange={(e) => handleChange(e)}></textarea>
+            <div className="buttons">
+                <Button variant="contained" color="secondary" onClick={() => setPopup(false)}>
+                    Close
+                </Button>
+                <Button variant="contained" color="primary" onClick={(e) => setData(e)}>
+                    Add
+                </Button>
             </div>
-        </Router>
+        </div>
+    );
+    return (
+        <div className="App">
+            <h1>Select And Mark Some Text</h1>
+            <div className="">
+                <div id="ele2" className="text" onMouseUp={(e) => mouseUp(e)}>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque equat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit
+                        id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, eque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam
+                        sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
+                    </p>
+                    <div id="aa" onClick={(e) => clicked(e)}>
+                        <Button variant="contained" color="secondary">
+                            +
+                        </Button>
+                    </div>
+                </div>
+                {popup ? popupDiv : null}
+            </div>
+        </div>
     );
 }
 
--- src/secondMethod.js
+++ src/secondMethod.js
@@ -5,6 +5,7 @@ function SecondMethod() {
     const [state, setState] = useState([]);
     const [text, setText] = useState('');
     const [key, setKey] = useState('');
+    const [selection, setSelection] = useState('');
     console.log(state);
 
     // push to github and eploy to github pages
@@ -16,11 +17,13 @@ function SecondMethod() {
 
         selection = window.getSelection();
         var p = selection.toString();
+
+        setSelection(p);
         // selection.toString() !== '' && alert(selection.toString() + e.pageX + '/' + e.pageY);
         if (p.length > 0) {
             aaa.style.display = 'block';
-            aaa.style.top = y - 210 + 'px';
-            aaa.style.left = x - 150 + 'px';
+            aaa.style.top = y - 200 + 'px';
+            aaa.style.left = x - 20 + 'px';
         }
         setKey(p);
     };
@@ -32,11 +35,19 @@ function SecondMethod() {
     const setData = (e) => {
         console.log(text);
         console.log(key);
-        setState(state.concat({ [key]: text }));
+        var d = new Date();
+        setState(
+            state.concat({
+                uuid: d.getTime() + Math.floor(Math.random() * 100),
+                writtenText: text,
+                selectedText: selection,
+            })
+        );
 
         setPopup(false);
         var ele = document.getElementById('aa');
         ele.style.display = 'none';
+        setText('');
     };
     const handleChange = (e) => {
         setText(e.target.value);
@@ -50,8 +61,8 @@ function SecondMethod() {
         </div>
     );
     return (
-        <div className="container mx-auto relative">
-            <div id="ele2" onMouseUp={(e) => mouseUp(e)}>
+        <div className="container mx-auto">
+            <div id="ele2" className="text" onMouseUp={(e) => mouseUp(e)}>
                 <p>
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque equat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id,
                     lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
@@ -61,10 +72,10 @@ function SecondMethod() {
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, eque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit
                     amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
                 </p>
+                <button id="aa" onClick={clicked}>
+                    +
+                </button>
             </div>
-            <button id="aa" onClick={clicked}>
-                +
-            </button>
             {popup ? popupDiv : null}
         </div>
     );
