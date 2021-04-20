528a15c9c817933fdca54fa474c9c4f562032f91
faraz3 <ch.faraz324@gmail.com>
2021-04-20 15:40:55 +0500

    saved to local storage and aded hover effect

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
     const [text, setText] = useState('');
     const [key, setKey] = useState('');
     const [selection, setSelection] = useState('');
+    const [hoverData, setHover] = useState('');
     const [paragraph, setParagraph] = useState(
         'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque equat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, eque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,'
     );
@@ -30,8 +31,12 @@ function App() {
             }
         });
     }, [selection, state]);
-
     useEffect(() => {
+        setState(JSON.parse(localStorage.getItem('data')));
+    }, []);
+    useEffect(() => {
+        let data = JSON.stringify(state);
+        localStorage.setItem('data', data);
         console.log(state);
     }, [state]);
 
@@ -73,7 +78,7 @@ function App() {
         // selection.toString() !== '' && alert(selection.toString() + e.pageX + '/' + e.pageY);
         if (p.length > 0 && !popup) {
             aaa.style.display = 'block';
-            aaa.style.top = y - 100 + 'px';
+            aaa.style.top = y - 110 + 'px';
             aaa.style.left = x - 90 + 'px';
         }
         setKey(p);
@@ -147,12 +152,24 @@ function App() {
 
     const hover = (e) => {
         let data = state.map((state) => {
-            return highlight.map((highlight) => {
-                return state.selectedText === highlight ? state.writtenText : null;
-            });
+                return state.selectedText.toUpperCase() === e.target.innerText.toUpperCase() ? state.writtenText : null;
         });
+        setHover(data);
         console.log(data.join(''));
         console.log(e.clientX, e.clientY);
+
+        var tip = document.getElementById('tip');
+        var x = e.clientX;
+        var y = e.clientY;
+
+        tip.style.display = 'block';
+        tip.style.top = y - 50 + 'px';
+        tip.style.left = x - 40 + 'px';
+    };
+    const mouseOut = (e) => {
+        var tip = document.getElementById('tip');
+        console.log('sssssssssssssssssssssssssssssssssssssssssss');
+        tip.style.display = 'none';
     };
     //paragraph.match(/h/g);
 
@@ -195,6 +212,40 @@ function App() {
     });
     var values = highlight.join('|');
 
+    //  const exportCSVFile=( items, fileTitle)=> {
+
+    //     const convertToCSV=(arr)=> {
+    //         const array = [Object.keys(arr[0])].concat(arr)
+
+    //         return array.map(it => {
+    //           return Object.values(it).toString()
+    //         }).join('\n')
+    //       }
+    //     // Convert Object to JSON
+    //     var jsonObject = JSON.stringify(items)
+
+    //     var csv = convertToCSV(items) ;
+
+    //     var exportedFilename = fileTitle + '.csv' || 'export.csv'
+
+    //     var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' })
+    //     if (navigator.msSaveBlob) { // IE 10+
+    //       navigator.msSaveBlob(blob, exportedFilename)
+    //     } else {
+    //       var link = document.createElement('a')
+    //       if (link.download !== undefined) { // feature detection
+    //         // Browsers that support HTML5 download attribute
+    //         var url = URL.createObjectURL(blob)
+    //         link.setAttribute('href', url)
+    //         link.setAttribute('download', exportedFilename)
+    //         link.style.visibility = 'hidden'
+    //         document.body.appendChild(link)
+    //         link.click()
+    //         document.body.removeChild(link)
+    //       }
+    //     }
+    //   }
+
     return (
         <div className="App">
             <h1>Select And Mark Some Text</h1>
@@ -218,7 +269,7 @@ function App() {
                                   pattern: new RegExp(`(${values})`, 'gi'),
                                   decorator: (highlight, index) => {
                                       return (
-                                          <span key={index} style={{ fontWeight: 'bold', background: 'yellow' }} onMouseOver={(e) => hover(e)}>
+                                          <span key={index} style={{ fontWeight: 'bold', background: 'yellow' }} onMouseOver={(e) => hover(e)} onMouseOut={mouseOut}>
                                               {highlight}
                                           </span>
                                       );
@@ -233,11 +284,15 @@ function App() {
                         </Button>
                     </div>
                 </div>
+                <div id="tip">{hoverData}</div>
                 {popup ? popupDiv : null}
                 <div className="download">
                     <Button variant="contained" color="secondary">
                         <CSVLink data={state}>Download</CSVLink>
                     </Button>
+                    {/* <Button variant="contained" color="secondary" onClick={()=>exportCSVFile(state, "data")}>
+                        Download 2
+                    </Button> */}
                 </div>
             </div>
         </div>
