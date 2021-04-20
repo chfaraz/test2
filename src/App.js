4ac84c44500419fde0c02e23fcc4f8bbe8eeefc6
faraz3 <ch.faraz324@gmail.com>
2021-04-20 16:43:59 +0500

    fixed styles

--- src/App.js
+++ src/App.js
@@ -301,7 +301,7 @@ function App() {
                                   pattern: new RegExp(`(${values})`, 'gi'),
                                   decorator: (highlight, index) => {
                                       return (
-                                          <span key={index} style={{ fontWeight: 'bold', background: 'yellow',  cursor: 'pointer' }} onClick={(e) => hover(e)}>
+                                          <span key={index} style={{ fontWeight: 'bold', background: 'yellow', cursor: 'pointer' }} onClick={(e) => hover(e)}>
                                               {highlight}
                                           </span>
                                       );
@@ -318,9 +318,18 @@ function App() {
                 </div>
                 <div ref={hoverRef} id="tip">
                     <div>
-                        <span onClick={deleteData}>X</span>
+                        <p>{hoverData}</p>
+                        <span onClick={deleteData}>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <line x1="4" y1="7" x2="20" y2="7" />
+                                <line x1="10" y1="11" x2="10" y2="17" />
+                                <line x1="14" y1="11" x2="14" y2="17" />
+                                <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                            </svg>
+                        </span>
                     </div>
-                    {hoverData}
                 </div>
                 {popup ? popupDiv : null}
                 <div className="download">
